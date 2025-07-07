9d25c224353291e8d3ac2e6006c5ce360f10d97c
Tritrum <tritriumpalad@gmail.com>
2025-07-07 16:42:30 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,26 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+/*// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  base: './'
+})*/
+export default defineConfig({
+  base: '/dist/',
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
